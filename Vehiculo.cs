33fe44e7b1c5972946ea10a242ae7366fa683ec6
Jaime Alfredo Perez Rivera <jaimealfredo987@gmail.com>
2026-06-26 12:51:44 -0600

    Añadir clase abstracta Vehiculo al proyecto
    Se ha creado un nuevo archivo `Vehiculo.cs` que define la clase abstracta `Vehiculo`. Esta clase incluye un campo público `HorasEstancia`, una propiedad privada `horasestancia`, un constructor y un método abstracto `Calcular()`. También se han añadido directivas `using` necesarias para su funcionamiento.

---
+++ Vehiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaMensualSegundo
+{
+    public abstract class Vehiculo
+    {
+        public int HorasEstancia;
+
+        private int horasestancia
+        { 
+            get { return HorasEstancia; }
+            set { HorasEstancia = value; }
+        }
+
+        public Vehiculo(int horas)
+        {
+            HorasEstancia = horas;
+        }
+
+        public abstract double Calcular();
+    }
+}
